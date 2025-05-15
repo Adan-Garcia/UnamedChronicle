a0fb4a876782fb24cfd10cc16390ec9be907ad10
Adan Garcia <adan.v.morataya@gmail.com>
2025-05-15 16:48:51 +0000

    May 15, 2025, 12:48 PM

--- src/ui/scripts/chat.gd
+++ src/ui/scripts/chat.gd
@@ -5,6 +5,6 @@ extends Control
 
 #a tick is x seconds from the date y i want to set the labels text int the format day of week mm/dd/yyy h:mm am/pm
 func _physics_process(_delta):
-	var tick = Global.Worldstate.CurrentWorldState.current_time
+	var tick: int = Global.Worldstate.CurrentWorldState.current_time
 
 	print(tick)
