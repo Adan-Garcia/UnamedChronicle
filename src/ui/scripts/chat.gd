304e3b0dfb0a90d13c018f7a94d1539fad28f6fa
Adan Garcia <adan.v.morataya@gmail.com>
2025-05-15 16:56:10 +0000

    May 15, 2025, 12:56 PM

--- src/ui/scripts/chat.gd
+++ src/ui/scripts/chat.gd
@@ -2,9 +2,20 @@ extends Control
 
 @onready var label: Label = $MarginContainer/VBoxContainer/Panel3/Label
 
+# Unix timestamp of your “base date” Y.
+# e.g. if Y is 2025-01-01 00:00 UTC then:
+const ORIGIN_UNIX := 1704067200
 
-#a tick is x seconds from the date y i want to set the labels text int the format day of week mm/dd/yyy h:mm am/pm
+
 func _physics_process(_delta):
-	var tick: int = Global.Worldstate.CurrentWorldState.current_time
+	# 1) get your raw tick
+	var tick_seconds = Global.Worldstate.CurrentWorldState.current_time
 
-	print(tick)
+	# 2) turn it into an actual Unix timestamp
+	var ts = ORIGIN_UNIX + tick_seconds
+
+	# 3) build a DateTime in local time
+	var dt = Time.get_datetime_string_from_unix_time(ts, true)
+
+	#
+	label.text = dt.format("%A %m/%d/%Y %I:%M %p")
