c6f543b8bd590d1f7de63aa8d27f4ea41bd3588c
Adan Garcia <adan.v.morataya@gmail.com>
2025-05-15 16:40:14 +0000

    May 15, 2025, 12:40 PM

--- src/ui/scripts/chat.gd
+++ src/ui/scripts/chat.gd
@@ -1,1 +1,8 @@
 extends Control
+
+@onready var label: Label = $MarginContainer/VBoxContainer/Panel3/Label
+
+
+func _process(_delta):
+	var tick = Global.Worldstate.CurrentWorldState.tick
+	print(tick)
