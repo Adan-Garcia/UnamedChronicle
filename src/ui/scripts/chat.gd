de1433b74a1cd881b87b31b12acf9ca2d8159ab6
Adan Garcia <adan.v.morataya@gmail.com>
2025-05-15 16:45:20 +0000

    May 15, 2025, 12:45 PM

--- src/ui/scripts/chat.gd
+++ src/ui/scripts/chat.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var label: Label = $MarginContainer/VBoxContainer/Panel3/Label
 
 
-func _process(_delta):
-	var tick = Global.Worldstate.CurrentWorldState.tick
+#a tick is x seconds from the date y i want to set the labels text int the format day of week mm/dd/yyy h:mm am/pm
+func _physics_process(_delta):
+	var tick = Global.Worldstate.CurrentWorldState.current_time
+
 	print(tick)
